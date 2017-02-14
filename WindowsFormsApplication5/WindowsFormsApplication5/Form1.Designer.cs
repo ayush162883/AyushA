b534d1e8826f6fc824f8ba46dfe2215f427bd59c
AYUSH RAJ ACHARYA <acharya.ayushraj@gmail.com>
2017-02-14 12:47:42 +0100

    Added simulation application and helpme.txt

---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resolutionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lowerRangeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.upperRangeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpMeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtNextSample = new System.Windows.Forms.TextBox();
+            this.txtNextLogging = new System.Windows.Forms.TextBox();
+            this.txtNoOfWritings = new System.Windows.Forms.TextBox();
+            this.txtLogFile = new System.Windows.Forms.TextBox();
+            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.btnSample = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(708, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resolutionToolStripMenuItem,
+            this.lowerRangeToolStripMenuItem,
+            this.upperRangeToolStripMenuItem});
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(80, 29);
+            this.settingToolStripMenuItem.Text = "Setting";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(74, 29);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpMeToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(61, 29);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // resolutionToolStripMenuItem
+            // 
+            this.resolutionToolStripMenuItem.Name = "resolutionToolStripMenuItem";
+            this.resolutionToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.resolutionToolStripMenuItem.Text = "Resolution";
+            // 
+            // lowerRangeToolStripMenuItem
+            // 
+            this.lowerRangeToolStripMenuItem.Name = "lowerRangeToolStripMenuItem";
+            this.lowerRangeToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.lowerRangeToolStripMenuItem.Text = "Lower range";
+            // 
+            // upperRangeToolStripMenuItem
+            // 
+            this.upperRangeToolStripMenuItem.Name = "upperRangeToolStripMenuItem";
+            this.upperRangeToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.upperRangeToolStripMenuItem.Text = "Upper range";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(211, 30);
+            this.aboutToolStripMenuItem1.Text = "About";
+            // 
+            // helpMeToolStripMenuItem
+            // 
+            this.helpMeToolStripMenuItem.Name = "helpMeToolStripMenuItem";
+            this.helpMeToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.helpMeToolStripMenuItem.Text = "Help me";
+            this.helpMeToolStripMenuItem.Click += new System.EventHandler(this.helpMeToolStripMenuItem_Click);
+            // 
+            // txtNextSample
+            // 
+            this.txtNextSample.Location = new System.Drawing.Point(249, 52);
+            this.txtNextSample.Name = "txtNextSample";
+            this.txtNextSample.Size = new System.Drawing.Size(100, 26);
+            this.txtNextSample.TabIndex = 1;
+            // 
+            // txtNextLogging
+            // 
+            this.txtNextLogging.Location = new System.Drawing.Point(243, 123);
+            this.txtNextLogging.Name = "txtNextLogging";
+            this.txtNextLogging.Size = new System.Drawing.Size(106, 26);
+            this.txtNextLogging.TabIndex = 2;
+            // 
+            // txtNoOfWritings
+            // 
+            this.txtNoOfWritings.Location = new System.Drawing.Point(249, 220);
+            this.txtNoOfWritings.Name = "txtNoOfWritings";
+            this.txtNoOfWritings.Size = new System.Drawing.Size(100, 26);
+            this.txtNoOfWritings.TabIndex = 3;
+            // 
+            // txtLogFile
+            // 
+            this.txtLogFile.Location = new System.Drawing.Point(79, 268);
+            this.txtLogFile.Name = "txtLogFile";
+            this.txtLogFile.Size = new System.Drawing.Size(100, 26);
+            this.txtLogFile.TabIndex = 4;
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Location = new System.Drawing.Point(368, 93);
+            this.txtHistory.Multiline = true;
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.Size = new System.Drawing.Size(328, 222);
+            this.txtHistory.TabIndex = 5;
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(79, 168);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(75, 39);
+            this.btnLog.TabIndex = 6;
+            this.btnLog.Text = "Logging";
+            this.btnLog.UseVisualStyleBackColor = true;
+            // 
+            // btnSample
+            // 
+            this.btnSample.Location = new System.Drawing.Point(79, 93);
+            this.btnSample.Name = "btnSample";
+            this.btnSample.Size = new System.Drawing.Size(75, 33);
+            this.btnSample.TabIndex = 7;
+            this.btnSample.Text = "Sampling";
+            this.btnSample.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Next Sampling";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Next Logging";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "No. Of Writings";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Log File Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(421, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Sensor Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 388);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSample);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.txtLogFile);
+            this.Controls.Add(this.txtNoOfWritings);
+            this.Controls.Add(this.txtNextLogging);
+            this.Controls.Add(this.txtNextSample);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resolutionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lowerRangeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem upperRangeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem helpMeToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtNextSample;
+        private System.Windows.Forms.TextBox txtNextLogging;
+        private System.Windows.Forms.TextBox txtNoOfWritings;
+        private System.Windows.Forms.TextBox txtLogFile;
+        private System.Windows.Forms.TextBox txtHistory;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Button btnSample;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
